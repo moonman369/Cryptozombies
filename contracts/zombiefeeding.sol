23fee4d8a4732d8861182f6cc32a597491ed6ccd
"moonman369" <"mightyayan369@gmail.com">
2022-04-11 23:21:35 +0530

    11.4.22-cz

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -21,6 +21,11 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
+  modifier ownerOf(uint _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    _;
+  }
+
   function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
@@ -29,9 +34,13 @@ contract ZombieFeeding is ZombieFactory {
     _zombie.readyTime = uint32(now + cooldownTime);
   }
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
-    require(msg.sender == zombieToOwner[_zombieId]);
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+      return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
@@ -46,5 +55,4 @@ contract ZombieFeeding is ZombieFactory {
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
-
 }
